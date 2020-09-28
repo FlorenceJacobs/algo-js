4bce30cd687f8d3d34a3982161f1e3a5407603e9
Florence Jacobs <florencejacobs85@gmail.com>
2020-09-28 11:55:12 +0200

    exo2

---
+++ js/1.1.js
@@ -0,0 +1,3 @@
+let myStateOfMind = "J'y arriverai, JS c'est la vie";
+
+console.log(myStateOfMind);
---
+++ js/1.2.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let name = "Florence";
+let firstName = "Jacobs";
+let city = "Villers-la-Ville";
+
+console.log("Your name is " + name + " " + firstName + " and you live in " + city);
