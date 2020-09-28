50d2fa243d13cae74c6a0dcd9270f019f1f1796f
Florence Jacobs <florencejacobs85@gmail.com>
2020-09-28 14:35:25 +0200

    exo6

--- js/1.6.js
+++ js/1.6.js
@@ -1,6 +1,6 @@
 const readlineSync = require("readline-sync");
 
-let decimalNmb1 = new Number(Math.trunc(readlineSync.question ("Please enter one integer :")));
-let decimalNmb2 = new Number(readlineSync.question ("Please enter a new integer :"));
+let IntegerA = new Number(Math.trunc(readlineSync.question ("Please enter one integer :")));
+let IntegerB = new Number(readlineSync.question ("Please enter a new integer :"));
 
-console.log("Returns: " + (decimalNmb1 * decimalNmb2);
+console.log("Returns: " + (IntegerA % IntegerB));
