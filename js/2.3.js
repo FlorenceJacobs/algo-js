0d4555624934e71f8364009ea8d2913a3125c290
Florence Jacobs <florencejacobs85@gmail.com>
2020-09-28 17:22:41 +0200

    exo2.3

---
+++ js/2.3.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+/*WHILE
+let i = 1
+    while (i<=100) {
+        i++;
+        if (i%2===0) {
+            console.log(i);
+        }
+    }
+*/
+
+for (let i = 2; i <=100; i+=2) {
+    console.log(i);
+}
---
+++ js/2.4.js
@@ -0,0 +1,2 @@
+Display all even numbers between 1 and 100.
+The first one should use while and make a loop that will execute 100 times. The second one should use for and make a loop that will execute 50 times.
